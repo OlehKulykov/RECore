6569a48e4f464b675d93fff2cabe35ae2c286c8b
OlehKulykov <nonamedemail@gmail.com>
2015-04-10 13:59:06 +0200

    File path tests

---
+++ test/test_recore_filepaths.cpp
@@ -0,0 +1,143 @@
+/*
+ *   Copyright (c) 2012 - 2015 Kulykov Oleh <info@resident.name>
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <assert.h>
+
+#if defined(CMAKE_BUILD)
+#undef CMAKE_BUILD
+#endif
+
+#if defined(__BUILDING_RECORE_DYNAMIC_LIBRARY__)
+#undef __BUILDING_RECORE_DYNAMIC_LIBRARY__
+#endif
+
+#define HAVE_ASSERT_H 1
+
+#include "../include/RECore.h"
+
+
+#if defined(HAVE_RECORE_CONFIG_H)
+#include "recore_config.h"
+#endif
+
+
+#if defined(CMAKE_BUILD)
+#undef CMAKE_BUILD
+#endif
+
+#include "../include/REUUIDv4.h"
+#include "../include/RELog.h"
+#include "../include/REMutableString.h"
+#include "../include/REString.h"
+
+
+int testPathExtension()
+{
+	REMutableString path;
+
+	path = "/dir/file";
+	if (path.pathExtension().isNotEmpty()) return EXIT_FAILURE;
+
+	path = "/dir/file.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path = "/dir/file.bak.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/dir/file.bak")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/dir/file")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/dir/file")) return EXIT_FAILURE;
+
+
+	
+	path = "/file";
+	if (path.pathExtension().isNotEmpty()) return EXIT_FAILURE;
+
+	path = "/file.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path = "/file.bak.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/file.bak")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/file")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("/file")) return EXIT_FAILURE;
+
+
+
+	path = "file";
+	if (path.pathExtension().isNotEmpty()) return EXIT_FAILURE;
+
+	path = "file.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path = "file.bak.ext";
+	if (!path.pathExtension().isEqual("ext")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("file.bak")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("file")) return EXIT_FAILURE;
+
+	path.removePathExtension();
+	if (!path.isEqual("file")) return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
+}
+
+int test1()
+{
+	REMutableString path;
+
+	REString s;
+
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[])
+{
+	RELog::log("Test testPathExtension ...");
+	int test = testPathExtension();
+	assert(test == EXIT_SUCCESS);
+	if (test != EXIT_SUCCESS) return EXIT_FAILURE;
+	RELog::log("Test testPathExtension OK");
+
+	RELog::log("All tests OK");
+
+	return EXIT_SUCCESS;
+}
+
